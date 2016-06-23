df19e7761a2e659d258c5abc7e7742f977c0ebd8
Gerard <gerard@wide-eyes.it>
2016-06-23 11:06:58 +0200

    1106

--- build/index.js
+++ build/index.js
@@ -3,12 +3,13 @@
 const React = require('react')
 const ReactDOM = require('react-dom')
 const Button = require('../lib').Button
+const Hero = require('../lib').Hero
 
 ReactDOM.render((
-  <div>
+  <Hero title='Example of title'>
     <div style={{marginBottom: '1rem'}}><Button size='bg' mode='primary'>This is a very long button</Button></div>
     <div style={{marginBottom: '1rem'}}><Button size='bg' mode='success'>This is a very long button</Button></div>
     <div style={{marginBottom: '1rem'}}><Button size='bg' mode='danger'>This is a very long button</Button></div>
     <div style={{marginBottom: '1rem'}}><Button size='bg' mode='warning'>This is a very long button</Button></div>
-  </div>
+  </Hero>
 ), document.getElementById('root'))
--- lib/hero.jsx
+++ lib/hero.jsx
@@ -1,19 +1,26 @@
-import hero from 'styles/hero.scss';
+import hero from 'styles/hero.scss'
 import React from 'react'
 
-export const Hero = ({title, style, children}) =>
-  <section className={hero.root} style={style || {}}>
-    {title
-      ? (
-        <header className={hero.header}>
-          <hgroup>
-            <h3 className="no-margin">{title}</h3>
-          </hgroup>
-        </header>
-      )
-      : null
-    }
-    <section>
-      {children}
+export const Hero = ({title, style, children}) => {
+  let header = null
+  if (title) {
+    header = (
+      <header className={hero.header}>
+        <hgroup>
+          <h3 className={hero.title}>
+            {title}
+          </h3>
+        </hgroup>
+      </header>
+    )
+  }
+
+  return (
+    <section className={hero.root} style={style || {}}>
+      {header}
+      <section className={hero.body}>
+        {children}
+      </section>
     </section>
-  </section>
+  )
+}
